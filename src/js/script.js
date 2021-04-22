338dcf1ce0593745210e7d47d478d6171ed27477
Bogdan <deadlynagel@ukr.net>
2021-04-22 21:03:06 +0200

    Fot Ivan

--- src/js/script.js
+++ src/js/script.js
@@ -14,7 +14,11 @@ $(document).ready(function() {
                 arrows: false
             }
         }]
+    });
 
-
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.tabs').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 });
